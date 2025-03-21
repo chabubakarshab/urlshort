0636ae27a85ae14fb2e02577ed3c2a2256285f0f
chabubakarshab <164537332+chabubakarshab@users.noreply.github.com>
2025-03-21 16:29:43 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,27 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      'scontent-lax.cdninstagram.com',
-      'scontent.cdninstagram.com',
-      'scontent.xx.fbcdn.net',
-      'scontent.fkhi5-1.fna.fbcdn.net',
-      'video-cdninstagram-com.translate.goog',
-      'scontent.flhe42-1.fna',
-      'scontent.flhe42-1.fna.fbcdn.net',
-      'scontent-lax3-1.cdninstagram.com',
-      'scontent-lax3-2.cdninstagram.com',
-      'instagram.flhe42-1.fna.fbcdn.net',
-      'instagram.fkhi5-1.fna.fbcdn.net',
-      'scontent.fhex4-1.fna.fbcdn.net',
-      'scontent.fhex4-2.fna.fbcdn.net',
-      'scontent.fhex4-1.fna',
-      'scontent.fhex4-2.fna',
-      // Generic patterns to catch all variations
-      'scontent-*.cdninstagram.com',
-      'scontent-*.fna.fbcdn.net',
-      'instagram.f*-*.fna.fbcdn.net',
-      'scontent.f*-*.fna.fbcdn.net',
-      'scontent.f*-*.fna'
+      'scontent-lax.cdninstagram.com'
     ],
   },
   // Add headers to prevent caching issues with Facebook crawler
